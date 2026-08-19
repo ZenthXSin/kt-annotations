453f45bf63b90d30ee5a0020d4c5cb051af6a67c
ZenthXSin <13437823821@163.com>
2026-08-19 15:27:55 +0800

    fix(buildSrc): publish ktannot-gradle-plugin with proper Gradle plugin marker
    - Add java-gradle-plugin + maven-publish to buildSrc for proper plugin publishing
    - Remove manual META-INF properties (auto-generated by gradlePlugin block)
    - Add group/version, gradlePlugin { register() }, and publishing config
    - Published POM kotlin-compiler-embeddable version must be manually patched to 2.2.0
      for compatibility with consuming projects using Kotlin 2.2.x

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     `kotlin-dsl`
+    `java-gradle-plugin`
+    `maven-publish`
 }
 
 repositories {
@@ -11,4 +13,26 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.0.21")
     implementation("com.squareup:kotlinpoet-jvm:1.17.0")
     implementation(kotlin("stdlib"))
+}
+
+group = "io.eve.ktannot"
+version = "0.1.0"
+
+gradlePlugin {
+    plugins {
+        register("ktannotPlugin") {
+            id = "io.eve.ktannot"
+            implementationClass = "io.eve.ktannot.KtAnnotationsPlugin"
+        }
+    }
+}
+
+publishing {
+    publications {
+        withType<MavenPublication>().configureEach {
+            pom {
+                name.set("ktannot-gradle-plugin")
+            }
+        }
+    }
 }
